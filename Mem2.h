ee34db19ff6a81dc23264a17ef11eac5e570aa04
Eugene Pavlov <eugenepavlov523@gmail.com>
2022-04-10 01:56:18 +0300

    Working on memory domains

--- Mem2.h
+++ Mem2.h
@@ -1,6 +1,8 @@
 #ifndef VIRGO_MEM2_H
 #define VIRGO_MEM2_H
 
+#include <array>
+#include <vector>
 #include <bitset>
 #include "Obj.h"
 
@@ -15,22 +17,46 @@ struct ObjHnd {
     std::bitset<32> flags {};
 };
 
-struct Ref2 {
+struct Reference {
     ObjHnd * objHndPtr {};
 
-    Ref2() = default;
-    explicit Ref2(ObjHnd * objHndPtr);
-    bool operator==(const Ref & another) const;
-    bool operator!=(const Ref & another) const;
-    bool operator<(const Ref & another) const;
-    static const Ref2 none;
+    Reference() = default;
+    explicit Reference(ObjHnd * objHndPtr);
+    bool operator==(const Reference & another) const;
+    bool operator!=(const Reference & another) const;
+    bool operator<(const Reference & another)  const;
+    static const Reference none;
 };
-
-const int MemDomainCapacity = 1024;
 
 struct MemDomain {
-    ObjHnd handlers[MemDomainCapacity];
-    unsigned int freeIndicesStack[MemDomainCapacity];
+    static const int MemDomainCapacity = (1 << 8); // 256
+    unsigned int generation {};
+    std::array<ObjHnd, MemDomainCapacity> handlers {};
+    std::array<unsigned int, MemDomainCapacity> freeIndicesStack {};
+    int freeIndicesStackTop {};
+
+    MemDomain();
+    void Init();
+    Ref NewRef();
+    void Mark();
+    void Sweep();
+    void CollectGarbage();
+    bool HasFreeHandlers();
 };
+
+struct Heap {
+    static std::vector<MemDomain*> domains;
+
+    // Temporary references stack is used during evaluating expressions.
+    // We push to this stack intermediate results of a currently evaluating expression,
+    // so we can be sure that intermediate values will not be deleted if GC will start
+    // in the middle of expression.
+    static const int RefStackCapacity = (1 << 10); // 1024
+    static std::array<Ref, RefStackCapacity> refStack;
+    static int refStackTop;
+
+    static void (*MarkCallback) ();
+};
+
 
 #endif //VIRGO_MEM2_H
